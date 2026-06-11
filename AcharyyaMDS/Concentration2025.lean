b921c63398c2eed15f8a5513d83b192fc81fc048
joncrall <jon.crall@kitware.com>
2026-06-11 13:49:32 -0400

    Remove deprecated AcharyyaMDS

--- AcharyyaMDS/Concentration2025.lean
+++
@@ -1,3 +0,0 @@
-/- Compatibility shim for older `AcharyyaMDS.Concentration2025` imports. -/
-
-import Acharyya2025.Concentration
--- AcharyyaMDS/Consistency2024.lean
+++
@@ -1,3 +0,0 @@
-/- Compatibility shim for older `AcharyyaMDS.Consistency2024` imports. -/
-
-import Acharyya2024.Consistency
